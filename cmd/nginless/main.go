34544ed104d3d4b55ebf87ce4dd53d7735949183
duanckham <duanckham@gmail.com>
2021-06-30 15:50:23 +0800

    FEAT: Logger

--- cmd/nginless/main.go
+++ cmd/nginless/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"github.com/duanckham/nginless/internal/app/nginless"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
-	logger, _ := zap.NewProduction()
+	// Log rotate.
+	// Refs:
+	// https://github.com/uber-go/zap/blob/master/FAQ.md#does-zap-support-log-rotation
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "/tmp/nginless.log",
+		MaxSize:    1000,
+		MaxBackups: 20,
+		MaxAge:     14,
+	})
+
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		w,
+		zap.InfoLevel,
+	))
+
 	defer logger.Sync()
 
 	n := nginless.New(nginless.Options{
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/d5/tengo/v2 v2.7.0
 	github.com/duanckham/go-pcre v0.0.0-20191122205722-613496bb8aff
 	go.uber.org/zap v1.16.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.2.8
 )
